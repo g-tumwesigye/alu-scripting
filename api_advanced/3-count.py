85513373d7bfe3c4aef723d6bf58699ab9bf4703
g-tumwesigye <g.tumwesigy@alustudent.com>
2023-11-06 14:16:55 +0000

    well_counted

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -6,22 +6,22 @@ import json
 import operator
 import requests
 
-
 def count_words(subreddit, word_list, after=None):
     """get all the keyword count"""
 
     if len(word_list) == 0:
         print(None)
         return
+
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    result = requests.get(url,
-                          headers=headers,
-                          params={"after": after},
-                          allow_redirects=False)
+    result = requests.get(url, headers=headers, params={"after": after}, allow_redirects=False)
+
     if result.status_code != 200:
         return None
+
     body = json.loads(result.text)
+
     if body["data"]["after"] is not None:
         newlist = word_list
         if type(word_list[0]) is str:
@@ -29,18 +29,13 @@ def count_words(subreddit, word_list, after=None):
             for i in word_list:
                 if not any(j['key'].lower() == i.lower() for j in temp):
                     temp.append({"key": i.lower(), "count": 0, "times": 1})
-                else:
-                    item = list(filter(
-                                lambda search: search['key'] == i.lower(),
-                                temp))
-                    if len(item) > 0:
-                        item[0]["times"] = item[0]["times"] + 1
             newlist = temp
+
         for i in newlist:
             for j in body["data"]["children"]:
-                for k in j["data"]["title"].lower().split():
-                    if i["key"] == k:
-                        i["count"] = i["count"] + 1
+                title_words = j["data"]["title"].lower().split()
+                i["count"] += title_words.count(i["key"])
+
         return count_words(subreddit, newlist, body["data"]["after"])
     else:
         newlist = word_list
@@ -49,24 +44,20 @@ def count_words(subreddit, word_list, after=None):
             for i in word_list:
                 if not any(j['key'].lower() == i.lower() for j in temp):
                     temp.append({"key": i.lower(), "count": 0, "times": 1})
-                else:
-                    item = list(filter(
-                                lambda search: search['key'] == i.lower(),
-                                temp))
-                    if len(item) > 0:
-                        item[0]["times"] = item[0]["times"] + 1
             newlist = temp
+
         for i in newlist:
             for j in body["data"]["children"]:
-                for k in j["data"]["title"].lower().split():
-                    if i["key"] == k:
-                        i["count"] = i["count"] + 1
+                title_words = j["data"]["title"].lower().split()
+                i["count"] += title_words.count(i["key"])
+
         key = operator.itemgetter("key")
         sorted_list = sorted(word_list, key=key)
         key = operator.itemgetter("count")
         sorted_list = sorted(sorted_list, key=key, reverse=True)
         word_list = sorted_list
+
         for i in sorted_list:
             if i["count"] > 0:
-                print("{}: {}".format(i["key"], i["count"] * i["times"]))
-        return
+                print("{}: {}".format(i["key"], i["count"] * i["times"])
+
