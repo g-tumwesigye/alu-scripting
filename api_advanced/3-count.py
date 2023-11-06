8f17b7b54c2cf14aa7016a66c37819962ae166b6
g-tumwesigye <g.tumwesigy@alustudent.com>
2023-11-06 14:18:36 +0000

    well_counted_api

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,63 +1,53 @@
 #!/usr/bin/python3
-
-"""search post function"""
-
+""" 3-count.py """
 import json
-import operator
 import requests
 
-def count_words(subreddit, word_list, after=None):
-    """get all the keyword count"""
 
-    if len(word_list) == 0:
-        print(None)
-        return
+def count_words(subreddit, word_list, after="", count=[]):
+    """ prints a sorted count of given keywords """
+
+    if after == "":
+        count = [0] * len(word_list)
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    result = requests.get(url, headers=headers, params={"after": after}, allow_redirects=False)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'User-Agent': 'Mozilla/5.0'})
 
-    if result.status_code != 200:
-        return None
+    if request.status_code == 200:
+        data = request.json()
 
-    body = json.loads(result.text)
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
 
-    if body["data"]["after"] is not None:
-        newlist = word_list
-        if type(word_list[0]) is str:
-            temp = []
-            for i in word_list:
-                if not any(j['key'].lower() == i.lower() for j in temp):
-                    temp.append({"key": i.lower(), "count": 0, "times": 1})
-            newlist = temp
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
 
-        for i in newlist:
-            for j in body["data"]["children"]:
-                title_words = j["data"]["title"].lower().split()
-                i["count"] += title_words.count(i["key"])
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
 
-        return count_words(subreddit, newlist, body["data"]["after"])
-    else:
-        newlist = word_list
-        if type(word_list[0]) is str:
-            temp = []
-            for i in word_list:
-                if not any(j['key'].lower() == i.lower() for j in temp):
-                    temp.append({"key": i.lower(), "count": 0, "times": 1})
-            newlist = temp
-
-        for i in newlist:
-            for j in body["data"]["children"]:
-                title_words = j["data"]["title"].lower().split()
-                i["count"] += title_words.count(i["key"])
-
-        key = operator.itemgetter("key")
-        sorted_list = sorted(word_list, key=key)
-        key = operator.itemgetter("count")
-        sorted_list = sorted(sorted_list, key=key, reverse=True)
-        word_list = sorted_list
-
-        for i in sorted_list:
-            if i["count"] > 0:
-                print("{}: {}".format(i["key"], i["count"] * i["times"])
-
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
+        else:
+            count_words(subreddit, word_list, after, count)
